a15ff32c55c9ba2b5333bad4c8494a4eef98a59d
Graci-antunes <86809391+Graci-antunes@users.noreply.github.com>
2022-09-22 22:13:00 -0300

    tarefa react e nestjs

---
+++ segunda_tarefa/src/tarefa.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Put, Param, Delete } from "@nestjs/common"
+import { ok } from "assert";
+
+@Controller()
+export class TarefaController{
+
+    tarefaLista = [];
+
+    @Get("/tarefa")
+    lestaTarefa(){
+        return this.tarefaLista;
+    }
+
+    @Put("/tarefa")
+    salvarTarefa(@Body() tarefa){
+        let index = this.tarefaLista.findIndex(t => t.codigo == tarefa.codigo);
+        if (index >= 0) {
+            this.tarefaLista[index].descricao = tarefa.descricao
+        } else {
+            this.tarefaLista.push(tarefa);
+        }
+        return "ok";
+    }
+
+    @Get("/tarefa/:codigo")
+    buscarPorCod(@Param() parametro){
+        console.log(parametro.codigo)
+        let tarefa = this.tarefaLista.find(tarefa => tarefa.codigo == parametro.codigo);
+        return tarefa;
+    }
+
+    @Delete("/tarefa/:codigo")
+    excluirTarefa(@Param() parametro){
+        let index = this.tarefaLista.findIndex(tarefa => tarefa.codigo == parametro.codigo);
+        this.tarefaLista.splice(index, 1);
+        return "ok";
+    }
+}
